725a59543124ae9e3e046e7782acc5e1a76827ba
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2021-05-04 00:46:02 -0500

    Add files via upload

---
+++ Helper/AppError.swift
@@ -0,0 +1,37 @@
+//
+//  AppError.swift
+//  OperationQueueDemo
+//
+//  Created by JungpyoHong on 5/4/21.
+//
+
+import Foundation
+
+enum AppError: Error {
+    case badUrl
+    case badResponse
+    case serverError
+    case noData
+    case parseError
+    case badRequest
+    case genericError(String)
+    
+    var errorMessage: String {
+        switch self {
+        case .badUrl:
+            return ""
+        case .badResponse:
+            return ""
+        case .serverError:
+            return ""
+        case .noData:
+            return ""
+        case .badRequest:
+            return ""
+        case .parseError:
+            return ""
+        case .genericError(let message):
+            return message
+        }
+    }
+}
