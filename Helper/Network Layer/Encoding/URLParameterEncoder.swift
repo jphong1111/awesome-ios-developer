fa937ab0f2cca80da3af67ed1af433f0e5efceee
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2021-05-19 23:13:58 -0500

    Update URLParameterEncoder.swift

--- Helper/Network Layer/Encoding/URLParameterEncoder.swift
+++ Helper/Network Layer/Encoding/URLParameterEncoder.swift
@@ -7,27 +7,21 @@
 
 import Foundation
 
-public struct URLParameterEncoder: ParameterEncoder {
-    public func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
+struct URLParameterEncoder: ParameterEncoder {
+    static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
+        guard let url = urlRequest.url else { throw AppError.badUrl }
         
-        guard let url = urlRequest.url else { throw NetworkError.missingURL }
-        
-        if var urlComponents = URLComponents(url: url,
-                                             resolvingAgainstBaseURL: false), !parameters.isEmpty {
-            
+        if var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false), !parameters.isEmpty {
             urlComponents.queryItems = [URLQueryItem]()
             
-            for (key,value) in parameters {
-                let queryItem = URLQueryItem(name: key,
-                                             value: "\(value)".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed))
+            for (key, value) in parameters {
+                let queryItem = URLQueryItem(name: key, value: "\(value)")
                 urlComponents.queryItems?.append(queryItem)
             }
             urlRequest.url = urlComponents.url
         }
-        
         if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
             urlRequest.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
         }
-        
     }
 }
