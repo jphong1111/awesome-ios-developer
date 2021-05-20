21af3d0920b20229323b8ab77c6e0c1b889482d2
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2021-05-19 23:12:43 -0500

    Update JSONParameterEncoder.swift

--- Helper/Network Layer/Encoding/JSONParameterEncoder.swift
+++ Helper/Network Layer/Encoding/JSONParameterEncoder.swift
@@ -7,16 +7,16 @@
 
 import Foundation
 
-public struct JSONParameterEncoder: ParameterEncoder {
-    public func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
+struct JSONParameterEncoder: ParameterEncoder {
+    static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
         do {
             let jsonAsData = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
             urlRequest.httpBody = jsonAsData
             if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
                 urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
             }
-        }catch {
-            throw NetworkError.encodingFailed
+        } catch {
+            throw AppError.encodingFail
         }
     }
 }
