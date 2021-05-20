1a19cfa35c80814c1804c61304028033835ef64f
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2021-05-19 23:15:53 -0500

    Update NetworkRouter.swift

--- Helper/Network Layer/Services/NetworkRouter.swift
+++ Helper/Network Layer/Services/NetworkRouter.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-protocol NetworkRouter: class {
+protocol NetworkRouter: AnyObject {
     associatedtype EndPoint: EndPointType
     func request<T: Decodable>(_ route: EndPoint, completion: @escaping NetworkRouterCompletion<T>)
     func cancel()
